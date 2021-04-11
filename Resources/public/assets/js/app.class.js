560efc444f7fc4ac5e323a4c12239dd6b54c2fbe
gabriel nadon <gnadon@bisscomm.com>
2021-04-11 16:27:50 -0400

    fix

--- Resources/public/assets/js/app.class.js
+++ Resources/public/assets/js/app.class.js
@@ -2,5 +2,23 @@
 $.Class({
     namespace: 'app',
 
+    initTiny: function() {
+        if ($('textarea.textarea-tiny-mce').length > 0) {
+            tinymce.init({
+                selector: 'textarea.textarea-tiny-mce',
+                height: 500,
+                menubar: false,
+                plugins: [
+                    'advlist autolink lists link anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'table paste code'
+                ],
+                toolbar: 'undo redo | ' +
+                    'bold italic | alignleft aligncenter ' +
+                    'alignright alignjustify | bullist numlist | ' +
+                    'removeformat code',
+            });
+        }
+    }
 });
 
